f4d166695ee6537eb8d00c893708eea11712485a
RaddedMC <mycooljames@hotmail.com>
2022-02-16 11:15:49 -0500

    Initial plug control code

---
+++ RPI/SwitchController.py
@@ -0,0 +1,18 @@
+# ES1050 T24HoopNet
+# SwitchController.py
+# Programmed by JamesN / Radded
+# Co-designed with JarrettB
+
+# TODO: Config files for this data
+motorpin1 = 1
+motorpin2 = 2
+plugip = "192.168.1.1"
+plugip2 = "192.168.2.1"
+
+def main():
+
+class SwitchController:
+
+
+if __name__ == "__main__":
+    main()
