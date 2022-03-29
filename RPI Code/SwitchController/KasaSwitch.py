a548e2144f64271ba82c45e5f2c188e38331a56b
RaddedMC <mycooljames@hotmail.com>
2022-03-29 19:24:15 -0400

    should work if internet dies

--- RPI Code/SwitchController/KasaSwitch.py
+++ RPI Code/SwitchController/KasaSwitch.py
@@ -6,6 +6,7 @@
 # DEPENDENCIES: pip install python-kasa
 
 import asyncio
+from tempfile import TemporaryFile
 from kasa import Discover
 import kasa
 
@@ -19,6 +20,7 @@ class KasaSwitch:
 
     ip_addr = None
     plug = None
+    __exists__ = True
 
     def __init__(self, ip_addr):
         """Initialize the plug object and connects to the smart plug at ip_addr_ on your network.
@@ -31,19 +33,35 @@ class KasaSwitch:
         try:
             self.plug = asyncio.run(Discover.discover_single(self.ip_addr))
         except kasa.exceptions.SmartDeviceException:
-            print("Unable to connect to the plug at " + self.plugip + ", continuing...")
+            print("Unable to connect to the plug at " + ip_addr + ", continuing...")
+            self.__exists__ = False
+
         
 
     def set_state(self, state):
         """Asynchronously turns your plug either on (state = True) or off (state = False).
         """
-        if (state):
-            asyncio.new_event_loop().run_until_complete(self.plug.turn_on())
+        if self.__exists__ == False:
+            try:
+                self.plug = asyncio.run(Discover.discover_single(self.ip_addr))
+                self.__exists__ = True
+            except:
+                pass
         else:
-            asyncio.new_event_loop().run_until_complete(self.plug.turn_off())
-        asyncio.new_event_loop().run_until_complete(self.plug.update())
+            if (state):
+                asyncio.new_event_loop().run_until_complete(self.plug.turn_on())
+            else:
+                asyncio.new_event_loop().run_until_complete(self.plug.turn_off())
+            asyncio.new_event_loop().run_until_complete(self.plug.update())
 
     def get_state(self) -> bool:
         """Gets the state of the plug -- is it on or off?
         """
-        return self.plug.is_on
+        if self.__exists__ == False:
+            try:
+                self.plug = asyncio.run(Discover.discover_single(self.ip_addr))
+                self.__exists__ = True
+            except:
+                pass
+        else:
+            return self.plug.is_on
